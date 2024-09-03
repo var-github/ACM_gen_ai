0029f0bcaa06aed18786150d46ce61a08908752c
Varun R <64779815+var-github@users.noreply.github.com>
2024-09-03 16:43:26 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,83 @@
+from groq import Groq
+import streamlit as st
+from huggingface_hub import InferenceClient
+
+api_GROQ = ""
+api_Hugging_Face = ""
+
+tab1, tab2 = st.tabs(["GROQ", "Hugging Face"])
+with tab1:
+    if 'context_tab1' not in st.session_state:
+        st.session_state['context_tab1'] = [{"role": "system", "content": "What can I help you with?"}]
+    chat_bot = Groq(api_key=api_GROQ)
+    st.header("Chatbot using GROQ")
+    for message in st.session_state["context_tab1"]:
+        if message["role"] == "system":
+            with st.chat_message("ai"):
+                st.write(message["content"])
+        else:
+            with st.chat_message("user"):
+                st.write(message["content"])
+    space_for_query = st.container()
+    query_input = st.empty()
+    query = query_input.chat_input("Enter query: ", key="tab1")
+    if query:
+        query_input.empty()
+        query_input.chat_input("Enter query: ", key="3", disabled=True)
+        with space_for_query.chat_message("user"):
+            st.write(query)
+        st.session_state['context_tab1'].append({"role": "user", "content": f"{query}"})
+        output = chat_bot.chat.completions.create(messages=st.session_state['context_tab1'], model="llama3-8b-8192", ).choices[0].message.content
+        st.session_state['context_tab1'].append({"role": "system", "content": output})
+        with space_for_query.chat_message("ai"):
+            st.write(output)
+        query_input.empty()
+        st.rerun()
+
+
+with tab2:
+    if 'context_tab2' not in st.session_state:
+        st.session_state['context_tab2'] = []
+    if 'chat_bot' not in st.session_state:
+        st.session_state['chat_bot'] = ""
+    st.header("Chatbot using Hugging Face")
+
+    def refresh():
+        st.session_state['context_tab2'] = []
+
+    models = ["mistralai/Mistral-Nemo-Instruct-2407", "mistralai/Mistral-7B-Instruct-v0.3",
+              "mistralai/Mistral-7B-Instruct-v0.2", "google/gemma-1.1-7b-it",
+              "meta-llama/Meta-Llama-3-8B-Instruct", "mistralai/Mixtral-8x7B-Instruct-v0.1",
+              "mistralai/Mistral-7B-Instruct-v0.1", "HuggingFaceH4/zephyr-7b-beta"]
+
+    model = st.selectbox("Which model to use for Chatbot ?", options=models, index=0, on_change=refresh)
+    st.session_state['chat_bot'] = InferenceClient(model=model, token=api_Hugging_Face)
+
+    def output(messages):
+        text = ""
+        for message in st.session_state['chat_bot'].chat_completion(messages=messages, stream=True, max_tokens=8000):
+            text = text + message.choices[0].delta.content
+        return text
+
+    with st.chat_message("ai"):
+        st.write("What can I help you with?")
+    for message in st.session_state["context_tab2"]:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+    space_for_query = st.container()
+    query_input = st.empty()
+    query = query_input.chat_input("Enter query: ", key="tab2")
+    if query:
+        query_input.empty()
+        query_input.chat_input("Enter query: ", key="2", disabled=True)
+        with space_for_query.chat_message("user"):
+            st.write(query)
+        st.session_state['context_tab2'].append({"role": "user", "content": f"{query}"})
+        with space_for_query:
+            with st.spinner("Generating response..."):
+                response = output(st.session_state['context_tab2'])
+        st.session_state['context_tab2'].append({"role": "assistant", "content": response})
+        with space_for_query.chat_message("ai"):
+            st.write(response)
+        query_input.empty()
+        st.rerun()
